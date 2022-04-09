d45b0e2a259ad397f6b8bb744501a33aaec40c36
alperenkarate <alperensozen96@gmail.com>
2022-04-09 22:43:54 +0300

    added auth comp binded inputs to state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from 'routes';
-import Topbar from 'components/Topbar';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "routes";
+import Topbar from "components/Topbar";
 
 const App = () => {
   return (
@@ -11,10 +11,11 @@ const App = () => {
       <AppRoutes />
     </Router>
   );
-}
+};
 
-
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
