ca25200ed2d2e727be01303f7743bcbccd216275
alperenkarate <alperensozen96@gmail.com>
2022-04-09 18:51:49 +0300

    react-router-dom is configured.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from 'routes';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>Welcome here is our app</h3>
+      <Router>
+        <AppRoutes />
+      </Router>
+    </div>
+  );
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
